11e5c5f970841c844d180606c7083ba9bc2e3532
VithuXD <84857358+VithuXD@users.noreply.github.com>
2021-05-29 17:05:55 -0400

    Add files via upload

---
+++ RoftCopter.java
@@ -0,0 +1,56 @@
+//////////
+//
+// V2 KUMAR GNANA5AM6
+// Le 9 fevrier 2021
+// Animation ASCII
+//
+//////////
+
+public class RoftCopter{
+    public static void main (String [] args){
+        
+        while (true){
+            
+            //Cadre ROFLCOPTER
+            
+            System.out.println("ROFL:ROFL:LOL:ROFL:ROFL");
+            System.out.println("           A           ");
+            System.out.println("  L    /--------       ");
+            System.out.println(" LOL====      []\\     ");
+            System.out.println("  L    \\          \\  ");
+            System.out.println("         \\________]   ");
+            System.out.println("            I   I      ");
+            System.out.println("          _________/   ");
+            System.out.println("                       ");
+            System.out.println("Hélicoptere de Vithu   ");
+            
+            //Cadre 1
+            
+            System.out.println("          LOL:ROFL:ROFL");
+            System.out.println("           A           ");
+            System.out.println("       /--------       ");
+            System.out.println(" LOL====      []\\     ");
+            System.out.println("  L    \\          \\  ");
+            System.out.println("         \\________]   ");
+            System.out.println("            I   I      ");
+            System.out.println("          _________/   ");
+            System.out.println("                       ");
+            System.out.println("Hélicoptere de Vithu   ");
+            
+            //Cadre 2
+            
+            System.out.println("ROFL:ROFL:LOL          ");
+            System.out.println("           A           ");
+            System.out.println("  L    /--------       ");
+            System.out.println(" LOL====      []\\     ");
+            System.out.println("       \\          \\  ");
+            System.out.println("         \\________]   ");
+            System.out.println("            I   I      ");
+            System.out.println("          _________/   ");
+            System.out.println("                       ");
+            System.out.println("Hélicoptere de Vithu   ");
+            
+            //thread.sleep(100);
+        }
+    }
+}
